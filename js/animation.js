560a3cfe3436627de75e1e58ef6429904c3dc456
Moeez Ahmad <moiz27673855@gmail.com>
2026-01-29 00:04:22 -0800

    New Project Added

--- js/animation.js
+++ js/animation.js
@@ -3,6 +3,6 @@ let element = document.querySelectorAll(".project-list > div")
 console.log(element);
 
 element.forEach(ele => {
-    ele.style.setProperty('--img', `-${ele.children[0].querySelector('img').offsetHeight-290}px`)
+    ele.style.setProperty('--img', `-${ele.children[0].querySelector('img').offsetHeight-280}px`)
 })
 
